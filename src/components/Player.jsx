6bc6e0ce0084db45fd7e74ec9c09e9f64363e857
Petermwansa <mwansapeter800@gmail.com>
2024-05-01 02:12:46 +0300

    Explore and revise using the state to render the entered input but it is still not ideal

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
+
 export default function Player() {
+
+  const [enteredName , setEnteredName] = useState('')
+  const [submitted , setSubmitted] = useState(false)
+
+  const hnadleChange = (e) => {
+    setSubmitted(false)
+    setEnteredName(e.target.value)
+  } 
+
+  const handleClick = () => {
+    setSubmitted(true)
+  }
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? enteredName : 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input type="text" onChange={hnadleChange} value={enteredName}/>
+        <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
   );
