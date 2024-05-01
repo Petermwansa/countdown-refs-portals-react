3afa006e017a61abbf99975ded65ed2293a2ef25
Petermwansa <mwansapeter800@gmail.com>
2024-05-01 04:24:07 +0300

    use useRef to handle the timers and the start and stop buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import Player from './components/Player.jsx';
+import TimerChallenge from './components/TimerChallenge.jsx';
 
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimerChallenge title='Soft' targetTime={1}/>
+        <TimerChallenge title='Less Soft' targetTime={5}/>
+        <TimerChallenge title='Hard' targetTime={7}/>
+        <TimerChallenge title='Very Hard' targetTime={10}/>
+        <TimerChallenge title='Erectly Hard' targetTime={15}/>
+      </div>
     </>
   );
 }
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,23 +1,19 @@
-import { useState } from "react";
+import { useState, useRef} from "react";
 
 export default function Player() {
+  const playerName = useRef();
 
   const [enteredName , setEnteredName] = useState('')
-  const [submitted , setSubmitted] = useState(false)
-
-  const hnadleChange = (e) => {
-    setSubmitted(false)
-    setEnteredName(e.target.value)
-  } 
 
   const handleClick = () => {
-    setSubmitted(true)
+    setEnteredName(playerName.current.value)
   }
+  
   return (
     <section id="player">
-      <h2>Welcome {submitted ? enteredName : 'unknown entity'}</h2>
+      <h2>Welcome {enteredName ?? 'unknown entity'}</h2>
       <p>
-        <input type="text" onChange={hnadleChange} value={enteredName}/>
+        <input ref={playerName} type="text"/>
         <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
